48c9ee124dd74e5464c73e3aa81c4028916eb365
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2021-08-12 12:04:32 +0600

    0.0v - Just Upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleFetch = async () => {
+        try {
+            const response = await fetch(url);
+            const finalRes = await response.json();
+            setData(finalRes);
+            setLoading(false);
+        } catch (err) {
+            console.log("there is an error");
+        }
+    };
+    useEffect(() => {
+        handleFetch();
+    }, []);
+
+    if (loading) {
+        return (
+            <section className="section loading">
+                <h1>Loading...</h1>
+            </section>
+        );
+    }
+
+    const { company, dates, duties, title } = data[currentIndex];
+
+    return (
+        <div className="container">
+            <div className="wrapper">
+                <div className="title">
+                    <h2>experience</h2>
+                </div>
+
+                <div className="content-wrapper">
+                    <div className="btn-container">
+                        {data.map((value, index) => {
+                            return (
+                                <button
+                                    key={value.id}
+                                    className={`btn ${
+                                        currentIndex === index && "active"
+                                    }`}
+                                    onClick={() => setCurrentIndex(index)}
+                                >
+                                    {value.company}
+                                </button>
+                            );
+                        })}
+                    </div>
+
+                    <div className="info-container">
+                        <h3>{title}</h3>
+                        <h4>{company}</h4>
+                        <p>{dates}</p>
+                        {duties.map((value, index) => {
+                            return (
+                                <div className="job-des" key={index}>
+                                    <FaAngleDoubleRight className="icn" />
+                                    <p>{value}</p>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
